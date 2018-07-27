144f530ff1471c10e15e043cc527efe4e0728b2c
Shuo Zhou <szhou20@sheffield.ac.uk>
2018-07-27 20:44:52 +0100

    update multi-class clf

--- experiment/4.0_wholebrain_task_classification.py
+++ experiment/4.0_wholebrain_task_classification.py
@@ -16,39 +16,13 @@ OPTIONS:
 from sklearn.svm import LinearSVC,SVC
 from sklearn.model_selection import StratifiedKFold,LeaveOneOut
 #import da_tool.tca
-import pickle
 import numpy as np
-import sys,os
-import getopt
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix,accuracy_score,classification_report
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 import itertools
+from utils.cmdline import commandline
+from utils import load_data
 
-
-class commandline:
-    def __init__(self):
-        opts,args = getopt.getopt(sys.argv[1:],'b:h')
-        opts = dict(opts)        
-        # default do not submit jod to HPC
-        self.basedir = '/shared/tale2/Shared/szhou/openfmri/preprocessed/whole/'
-        
-        if '-h' in opts:
-            self.printHelp()
-
-        if '-b' in opts:
-            self.basedir = opts['-b']
-            if self.basedir[-1]!='/':
-                self.basedir+='/';
-            if not os.path.exists(self.basedir):
-                print 'basedir %s does not exist!'%self.basedir
-                self.printHelp()
-
-            
-    def printHelp(self):
-        helpinfo = __doc__.replace('<PROGRAM>',sys.argv[0],1)
-        print helpinfo
-        print sys.stderr
-        sys.exit()
 
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
@@ -84,33 +58,26 @@ def plot_confusion_matrix(cm, classes,
     plt.xlabel('Predicted Label') 
     plt.tight_layout()
     plt.ylabel('True Label')
-    plt.savefig('cm11.pdf', format='pdf', dpi=1000)      
+#    plt.savefig('cm11.pdf', format='pdf', dpi=1000)      
         
 
 config = commandline()
-#icadir = config.icadir
-basedir = config.basedir
-# load data
 
+# load data
 task_ids = [1,2,3,4,5,6,8,9,10,21,22]
 
-data_run1=np.load(os.path.join(basedir,'zstat_run1.npy'))  
-data_run2=np.load(os.path.join(basedir,'zstat_run2.npy'))  
-data = np.hstack((data_run1,data_run2))
-
-labels_run1=np.loadtxt(os.path.join(basedir,'data_key_run1.txt'))[:,0]
-labels_run2=np.loadtxt(os.path.join(basedir,'data_key_run2.txt'))[:,0]
-labels = np.hstack((labels_run1,labels_run2))
-
-#data=np.load(os.path.join(basedir,'zstat_run1.npy'))  
-#labels=np.loadtxt(os.path.join(basedir,'data_key_run1.txt'))[:,0]
-data = data.T
+if config.clf_ica:
+    data, labels= load_data.load_ica(dim = config.dim, data_path=config.ica_dir,
+                                    label_path = config.label_dir)
+else:
+    data, labels = load_data.load_whole_brain(data_path = config.wb_dir,
+                                             label_path = config.label_dir)
 
 #initilize accuracy
 acc={}
 
-
-class_names = ['Task 1','Task 2','Task 3','Task 4','Task 5','Task 6','Task 8','Task 9','Task 10','Task 21','Task 22',]
+class_names = ['Task 1','Task 2','Task 3','Task 4','Task 5','Task 6','Task 8',
+               'Task 9','Task 10','Task 21','Task 22',]
 
 # 10 fold cross-validation
 skf = StratifiedKFold(n_splits=10)
@@ -121,13 +88,13 @@ for train, test in skf.split(data,labels):
     svm.fit(data[train],labels[train])
     whole_pred[test] = svm.predict(data[test])
 acc['whole'] = accuracy_score(whole_pred,labels)  
-print classification_report(labels,whole_pred)
+#print(classification_report(labels,whole_pred))
 cm =  confusion_matrix(labels,whole_pred)
 # Plot normalized confusion matrix
 plt.figure()
 plot_confusion_matrix(cm, classes=class_names, #normalize=True,
                       title='Confusion matrix (Whole-brain data classification)')
-plt.savefig('cm1.pdf', format='pdf', dpi=1000)
+plt.savefig('cm.pdf', format='pdf', dpi=1000)
 plt.show()
 
 
